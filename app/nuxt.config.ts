5caee8ea9b78e08a8684c4f746f2ae7eb95479c0
Felix Egbert <fekiegbert@googlemail.com>
2025-06-16 18:08:13 +0200

    feat: added linting

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/icon', '@nuxt/fonts']
+  modules: [
+    '@nuxt/ui',
+    '@nuxt/icon',
+    '@nuxt/fonts',
+    '@nuxt/eslint',
+  ],
 })
