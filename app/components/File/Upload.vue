ca5127ac7277992f096cd3de9e63d8487e3c15c3
Felix Egbert <fekiegbert@googlemail.com>
2025-09-12 21:00:20 +0200

    feat: added support for multiple uploads at once

---
+++ app/components/File/Upload.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { FileWithHandle } from 'browser-fs-access'
+import { fileOpen } from 'browser-fs-access'
+
+const files = defineModel<FileWithHandle[]>()
+
+async function openFile() {
+  const blob = await fileOpen({
+    mimeTypes: ['image/*'],
+    startIn: 'pictures',
+    multiple: true,
+  })
+
+  files.value = Array.isArray(blob) ? blob : [blob]
+}
+</script>
+
+<template>
+  <div>
+    <button @click="openFile">
+      Upload Files
+    </button>
+  </div>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { FileWithHandle } from 'browser-fs-access'
+
+const files = defineModel<FileWithHandle[]>()
+</script>
+
+<template>
+  <div>
+    Welcome to the IPTC Web Editor!
+
+    <div>
+      <FileUpload v-model="files" />
+      <FileDisplay :files="files ?? []" />
+    </div>
+  </div>
+</template>
