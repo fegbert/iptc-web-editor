16152efa686935b22f948b2d4f3d56310e7e2a87
Felix Egbert <fekiegbert@googlemail.com>
2025-10-05 23:53:42 +0200

    feat: improved types to handle more special cases

--- app/utils/iptc-iim/types.ts
+++ app/utils/iptc-iim/types.ts
@@ -1,12 +1,33 @@
 type IPTCFieldKey = `${number}:${number}`
-type IPTCFieldType = 'text' | 'number' | 'datetime' | 'date' | 'time'
 
-export interface IPTCField {
+export type FieldTypes = 'text' | 'number' | 'datetime' | 'date' | 'time' | 'select' | 'textarea' | 'subject-reference'
+
+type CharacterTypes = 'graphic' | 'space' | 'alphabetic' | 'numeric' | 'return' | 'linefeed'
+
+export interface IPTCFieldBase {
+  key: IPTCFieldKey
+  type: FieldTypes
   title: string
   description: string
-  type: IPTCFieldType
+  octets: { min?: number, max?: number }
   mandatory: boolean
   repeatable: boolean
+  parent?: IPTCFieldKey
+  deprecated?: boolean
 }
 
-export type IPTCFields = Record<IPTCFieldKey, IPTCField>
+export interface IPTCFieldText extends IPTCFieldBase {
+  type: 'text' | 'textarea'
+  allowedCharacterTypes: CharacterTypes
+}
+
+export interface IPTCFieldNumber extends IPTCFieldBase {
+  type: 'number'
+  minValue?: number
+  maxValue?: number
+}
+
+export interface IPTCFieldSelect extends IPTCFieldBase {
+  type: 'select'
+  options: { value: string, label: string }[]
+}
