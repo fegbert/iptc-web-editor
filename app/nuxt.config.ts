96f78cb2b297c7df70577e6794b637bb03622688
Felix Egbert <fekiegbert@googlemail.com>
2025-11-30 19:25:48 +0100

    chore: updated save to automatically get name and version from package.json instead of env file

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,7 +1,16 @@
+import { defineNuxtConfig } from 'nuxt/config'
+import { name, version } from './package.json'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: '2025-11-30',
   devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      appName: name,
+      appVersion: version,
+    },
+  },
   modules: [
     '@nuxt/ui',
     '@nuxt/icon',
